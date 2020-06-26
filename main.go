d86c7eec024a7e8c66a60434fe618d608bd4907e
Aaron Ellington <aaron@ellington.io>
2020-06-26 08:13:53 -0400

    just the start

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+
+	"github.com/coma-toast/supportctl/cmd/hello"
+	"github.com/coma-toast/supportctl/pkg/core"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	// Build the production Command Context
+	cmdCtx := core.CmdCtx{
+		StdOut: os.Stdout,
+		StdIn:  os.Stdin,
+	}
+
+	// Setup the Root Command
+	rootCmd := &cobra.Command{
+		Use:   "supportctl",
+		Short: "Make Techctl Great Again",
+	}
+
+	// Add and Setup the Hello Command
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "hello",
+		Short: "Just say Hello",
+		Run: func(cmd *cobra.Command, args []string) {
+			helloCmd := hello.Cmd{}
+			helloCmd.Run(cmdCtx)
+		},
+	})
+
+	// Run the Root Command
+	rootCmd.Execute()
+}
